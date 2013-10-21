12b258bd91d82f92abe02c2d47071db5a4646c44
Robert Carlsen <rwcarlsen@gmail.com>
2013-10-21 15:08:38 -0500

    added code to build a list of resource id inventory changes for an agent for all timesteps

--- inventory.go
+++ inventory.go
@@ -12,6 +12,7 @@ import (
 
 var fulltree = flag.Bool("res-tree", false, "output dot graph of entire resource tree")
 var inven = flag.Bool("inventory", false, "print time series of agent's resource id inventory")
+var changes = flag.Bool("changes", false, "print time series of changes to agent's resource id inventory")
 var qty = flag.Bool("qty", false, "show quantities in dot graph")
 
 func main() {
@@ -20,29 +21,47 @@ func main() {
 
 	fname := flag.Arg(0)
 	agentId, err := strconv.Atoi(flag.Arg(1))
-	if err != nil {
-		log.Fatal(err)
-	}
+	fatal(err)
 
 	conn, err := sqlite3.Open(fname)
-	if err != nil {
-		log.Fatal(err)
-	}
+	fatal(err)
 	defer conn.Close()
 
 	roots, err := BuildAgentGraph(conn, agentId)
-	if err != nil {
-		log.Fatal(err)
-	}
+	fatal(err)
 
-	if *inven {
-		// print time inventories
-		inventory, err := TimeInventory(conn, roots)
-		if err != nil {
-			log.Fatal(err)
+	if *fulltree {
+		roots, err := BuildResTree(conn)
+		fatal(err)
+
+		edges := EdgeSet{}
+		for _, node := range roots {
+			edges.Union(node.DotEdges())
 		}
+		fmt.Println(BuildDot(edges.Slice()))
+	} else if *inven {
+		inventory, err := TimeInventory(conn, roots)
+		fatal(err)
 		for t, set := range inventory {
 			fmt.Printf("timestep %v\n", t)
+			for node, _ := range set {
+				fmt.Printf("    %s\n", node)
+			}
+		}
+	} else if *changes {
+		added := map[int]map[*Node]bool{}
+		removed := map[int]map[*Node]bool{}
+		for _, node := range roots {
+			node.Changes(added, removed)
+		}
+		for time, set := range added {
+			fmt.Printf("Added at timestep %v\n", time)
+			for node, _ := range set {
+				fmt.Printf("    %s\n", node)
+			}
+		}
+		for time, set := range removed {
+			fmt.Printf("Removed at timestep %v\n", time)
 			for node, _ := range set {
 				fmt.Printf("    %s\n", node)
 			}
@@ -98,6 +117,27 @@ func (n *Node) AddChild(child *Node) {
 	child.Parent = n
 }
 
+func (n *Node) Changes(added, removed map[int]map[*Node]bool) {
+	n.changes(added, removed)
+}
+
+func (n *Node) changes(added, removed map[int]map[*Node]bool) {
+	if _, ok := added[n.Time]; !ok {
+		added[n.Time] = map[*Node]bool{}
+	}
+	if _, ok := removed[n.Time]; !ok {
+		removed[n.Time] = map[*Node]bool{}
+	}
+
+	added[n.Time][n] = true
+	if n.Parent != nil {
+		removed[n.Time][n.Parent] = true
+	}
+	for _, child := range n.Children {
+		child.changes(added, removed)
+	}
+}
+
 func (n *Node) ChoppedLeaves(choptime int) []*Node {
 	leaves := []*Node{}
 	if n.IsLeaf(choptime) {
@@ -135,16 +175,44 @@ func BuildDot(edges []string) string {
 	return buf.String()
 }
 
-func BuildAgentGraph(conn *sqlite3.Conn, agentId int) (roots []*Node, err error) {
-	// set up connection and create indexes
+func CreateIndex(conn *sqlite3.Conn) error {
 	sql := "CREATE INDEX IF NOT EXISTS res_par1 ON Resources(Parent1 ASC);"
 	sql += "CREATE INDEX IF NOT EXISTS res_par2 ON Resources(Parent2 ASC);"
-	if err := conn.Exec(sql); err != nil {
+	return conn.Exec(sql)
+}
+
+func BuildResTree(conn *sqlite3.Conn) (roots []*Node, err error) {
+	if err := CreateIndex(conn); err != nil {
+		return nil, err
+	}
+
+	// create root and end nodes
+	sql := "SELECT ID,TimeCreated,Quantity,Parent1,Parent2 FROM Resources"
+	for stmt, err := conn.Query(sql); err == nil; err = stmt.Next() {
+		p1, p2 := 0, 0
+		if err := stmt.Scan(nil, nil, nil, &p1, &p2); err != nil {
+			return nil, err
+		}
+		if p1 == 0 && p2 == 0 {
+			root := &Node{}
+			if err := stmt.Scan(&root.Id, &root.Time, &root.Qty); err != nil {
+				return nil, err
+			}
+			roots = append(roots, root)
+			addChildren(conn, root, map[int]bool{})
+		}
+	}
+
+	return roots, nil
+}
+
+func BuildAgentGraph(conn *sqlite3.Conn, agentId int) (roots []*Node, err error) {
+	if err := CreateIndex(conn); err != nil {
 		return nil, err
 	}
 
 	// get all resources transacted to/from an agent and when the tx occured
-	sql = `SELECT trr.ResourceID,tr.Time,tr.ReceiverID FROM Transactions AS tr
+	sql := `SELECT trr.ResourceID,tr.Time,tr.ReceiverID FROM Transactions AS tr
            INNER JOIN TransactedResources AS trr
              ON tr.ID = trr.TransactionID
            WHERE tr.SenderID = ? OR tr.ReceiverID = ?;`
@@ -229,4 +297,10 @@ func TimeInventory(conn *sqlite3.Conn, roots []*Node) (map[int]map[*Node]bool, e
 		}
 	}
 	return inventory, nil
+}
+
+func fatal(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
 }
