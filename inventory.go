c862c23db0dc74dccb52a4e4b58dffb585e8e82c
Robert Carlsen <rwcarlsen@gmail.com>
2013-10-24 11:56:15 -0500

    fixed slowness by adding missing index. Added some concurrency and fixed many bugs

--- inventory.go
+++ inventory.go
@@ -8,6 +8,7 @@ import (
 	"math"
 	"os"
 	"runtime/pprof"
+	"time"
 
 	"code.google.com/p/go-sqlite/go1/sqlite3"
 )
@@ -51,24 +52,36 @@ func main() {
 	roots, err := GetRoots(conn)
 	fatal(err)
 	fmt.Printf("Found %v root nodes\n", len(roots))
-	allNodes := make([][]*Node, len(roots))
-	count := 0
-	for i, root := range roots {
-		fmt.Printf("Processing root %d...\n", i)
-		nodes, err := WalkDown(conn, root)
-		fmt.Printf("  query_count = %d\n", queryCount)
-		fatal(err)
-		allNodes[i] = nodes
-		count += len(nodes)
-		if count >= dumpfreq {
-			fmt.Println("dumping to inventories table...")
-			fatal(DumpNodes(conn, allNodes))
-			count = 0
-			allNodes = allNodes[:0]
+
+	// row creating loop
+	ch := make(chan *Node)
+	unpause := make(chan bool)
+	go func() {
+		for i, root := range roots {
+			fmt.Printf("Processing root %d...\n", i)
+			err := WalkDown(conn, root, ch, unpause)
+			fatal(err)
 		}
+		close(ch)
+	}()
+
+	// row writing loop
+	writeconn, err := sqlite3.Open(fname)
+	fatal(err)
+	defer writeconn.Close()
+
+	nodes := make([]*Node, 0, 100000)
+	for node := range ch {
+		nodes = append(nodes, node)
+		if len(nodes) >= dumpfreq {
+			fmt.Println("dumping to inventories table...")
+			fatal(DumpNodes(writeconn, nodes))
+			nodes = nodes[:0]
+		}
+		unpause <- true
 	}
 	fmt.Println("dumping to inventories table...")
-	fatal(DumpNodes(conn, allNodes))
+	fatal(DumpNodes(writeconn, nodes))
 }
 
 func CreateNodeTable(conn *sqlite3.Conn) (err error) {
@@ -79,7 +92,7 @@ func CreateNodeTable(conn *sqlite3.Conn) (err error) {
 
 var dumpStmt *sqlite3.Stmt
 
-func DumpNodes(conn *sqlite3.Conn, nodes [][]*Node) (err error) {
+func DumpNodes(conn *sqlite3.Conn, nodes []*Node) (err error) {
 	if dumpStmt == nil {
 		dumpStmt, err = conn.Prepare("INSERT INTO Inventories VALUES (?,?,?,?)")
 		if err != nil {
@@ -90,11 +103,9 @@ func DumpNodes(conn *sqlite3.Conn, nodes [][]*Node) (err error) {
 	if err := conn.Exec("BEGIN TRANSACTION;"); err != nil {
 		return err
 	}
-	for _, group := range nodes {
-		for _, n := range group {
-			if err = dumpStmt.Exec(n.ResId, n.OwnerId, n.StartTime, n.EndTime); err != nil {
-				return err
-			}
+	for _, n := range nodes {
+		if err = dumpStmt.Exec(n.ResId, n.OwnerId, n.StartTime, n.EndTime); err != nil {
+			return err
 		}
 	}
 	if err := conn.Exec("END TRANSACTION;"); err != nil {
@@ -131,34 +142,36 @@ func GetRoots(conn *sqlite3.Conn) (roots []*Node, err error) {
 
 var mappednodes = map[int32]struct{}{}
 var resStmt *sqlite3.Stmt
-var queryCount = 0
+var nodeCount = 0
 
-func WalkDown(conn *sqlite3.Conn, node *Node) (nodes []*Node, err error) {
+func WalkDown(conn *sqlite3.Conn, node *Node, ch chan *Node, unpause chan bool) (err error) {
 	if _, ok := mappednodes[int32(node.ResId)]; ok {
 		return
 	}
 	mappednodes[int32(node.ResId)] = struct{}{}
-	queryCount += 2
+	nodeCount++
+	if nodeCount % 10000 == 0 {
+		fmt.Printf("%d resources done\n", nodeCount)
+	}
 
 	if resStmt == nil {
 		sql := "SELECT ID,TimeCreated FROM Resources WHERE Parent1 = ? OR Parent2 = ?"
 		resStmt, err = conn.Prepare(sql)
 		if err != nil {
-			return nil, err
+			return err
 		}
 	}
 
-	count := 0
 	kids := make([]*Node, 0, 2)
 	for err = resStmt.Query(node.ResId, node.ResId); err == nil; err = resStmt.Next() {
 		child := &Node{EndTime: math.MaxInt32}
 		if err := resStmt.Scan(&child.ResId, &child.StartTime); err != nil {
-			return nil, err
+			return err
 		}
 
 		owners, times, err := GetNewOwners(conn, node.ResId)
 		if err != nil {
-			return nil, err
+			return err
 		}
 		if len(owners) > 0 {
 			node.EndTime = times[0]
@@ -166,7 +179,8 @@ func WalkDown(conn *sqlite3.Conn, node *Node) (nodes []*Node, err error) {
 
 			times = append(times, child.StartTime)
 			for i := range owners {
-				nodes = append(nodes, &Node{ResId: node.ResId, OwnerId: owners[i], StartTime: times[i], EndTime: times[i+1]})
+				ch <-&Node{ResId: node.ResId, OwnerId: owners[i], StartTime: times[i], EndTime: times[i+1]}
+				<-unpause
 			}
 		} else {
 			node.EndTime = child.StartTime
@@ -174,21 +188,20 @@ func WalkDown(conn *sqlite3.Conn, node *Node) (nodes []*Node, err error) {
 		}
 
 		kids = append(kids, child)
-		count++
 	}
 	if err != io.EOF {
-		return nil, err
+		return err
 	}
 
 	for _, child := range kids {
-		subnodes, err := WalkDown(conn, child)
+		err := WalkDown(conn, child, ch, unpause)
 		if err != nil {
-			return nil, err
+			return err
 		}
-		nodes = append(nodes, subnodes...)
 	}
-	nodes = append(nodes, node)
-	return nodes, nil
+	ch<-node
+	<-unpause
+	return nil
 }
 
 var ownerStmt *sqlite3.Stmt
@@ -205,12 +218,14 @@ func GetNewOwners(conn *sqlite3.Conn, id int) (owners, times []int, err error) {
 	}
 
 	var owner, t int
-	for err = ownerStmt.Query(id); err == nil; err = ownerStmt.Next() {
+	err = ownerStmt.Query(id)
+	for err == nil {
 		if err := ownerStmt.Scan(&owner, &t); err != nil {
 			return nil, nil, err
 		}
 		owners = append(owners, owner)
 		times = append(times, t)
+		err = ownerStmt.Next()
 	}
 	if err != io.EOF {
 		return nil, nil, err
@@ -223,8 +238,10 @@ func CreateIndex(conn *sqlite3.Conn) error {
 	        CREATE INDEX IF NOT EXISTS res_par1 ON Resources(Parent1 ASC);
 	        CREATE INDEX IF NOT EXISTS res_par2 ON Resources(Parent2 ASC);
 	        CREATE INDEX IF NOT EXISTS trans_id ON Transactions(ID ASC);
+	        CREATE INDEX IF NOT EXISTS trans_time ON Transactions(Time ASC);
 	        CREATE INDEX IF NOT EXISTS trans_receiver ON Transactions(ReceiverID ASC);
 	        CREATE INDEX IF NOT EXISTS transres_transid ON TransactedResources(TransactionID ASC);
+	        CREATE INDEX IF NOT EXISTS transres_resid ON TransactedResources(ResourceID ASC);
 	        CREATE INDEX IF NOT EXISTS rescreate_resid ON ResCreators(ResID ASC);`
 	return conn.Exec(sql)
 }
@@ -232,5 +249,33 @@ func CreateIndex(conn *sqlite3.Conn) error {
 func fatal(err error) {
 	if err != nil {
 		log.Fatal(err)
+	}
+}
+
+type Timer struct {
+	Totals map[string]time.Duration
+	starts map[string]time.Time
+}
+
+func NewTimer() *Timer {
+	return &Timer{
+		Totals: make(map[string]time.Duration),
+		starts: make(map[string]time.Time),
+	}
+}
+
+func (t *Timer) Start(label string) {
+	t.starts[label] = time.Now()
+}
+
+func (t *Timer) Total(label string) time.Duration {
+	return t.Totals[label]
+}
+
+func (t *Timer) Stop(label string) {
+	stop := time.Now()
+	if start, ok := t.starts[label]; ok {
+		t.Totals[label] += stop.Sub(start)
+		delete(t.starts, label)
 	}
 }
