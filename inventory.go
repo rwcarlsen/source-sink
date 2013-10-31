04cc5140de6c6f3fd363eeb755d05e5114ebf8e1
Robert Carlsen <rwcarlsen@gmail.com>
2013-10-31 15:19:48 -0500

    tmp

--- inventory.go
+++ inventory.go
@@ -23,19 +23,95 @@ func main() {
 	fatal(err)
 	defer conn.Close()
 
-	ctx := &Context{Conn: conn}
-	fatal(ctx.Init())
-	defer ctx.Finish()
+	fatal(Prepare(conn))
+	defer Finish(conn)
 
-	fmt.Println("Retrieving root resource nodes...")
-	roots, err := GetRoots(conn)
+	simids, err := GetSimIds(conn)
 	fatal(err)
-	fmt.Printf("Found %v root nodes\n", len(roots))
 
-	for i, root := range roots {
-		fmt.Printf("Processing root %d...\n", i)
-		fatal(ctx.WalkDown(root))
+	for _, simid := range simids {
+		ctx := &Context{Conn: conn, Simid: simid}
+		fatal(ctx.WalkAll())
 	}
+}
+
+func GetSimIds(conn *sqlite3.Conn) (ids []string, err error) {
+	sql := "SELECT SimID FROM SimulationTimeInfo"
+	var stmt *sqlite3.Stmt
+	for stmt, err = conn.Query(sql); err == nil; err = stmt.Next() {
+		var s string
+		if err := stmt.Scan(&s); err != nil {
+			return nil, err
+		}
+		ids = append(ids, s)
+	}
+	if err != io.EOF {
+		return nil, err
+	}
+	return ids, nil
+}
+
+var (
+	preExecStmts = []string{
+		"CREATE TABLE IF NOT EXISTS Inventories (SimID TEXT,ResID INTEGER,AgentID INTEGER,StartTime INTEGER,EndTime INTEGER);",
+		"CREATE INDEX IF NOT EXISTS res_id ON Resources(SimID ASC,ID ASC);",
+		"CREATE INDEX IF NOT EXISTS res_par1 ON Resources(SimID ASC,Parent1 ASC);",
+		"CREATE INDEX IF NOT EXISTS res_par2 ON Resources(SimID ASC,Parent2 ASC);",
+		"CREATE INDEX IF NOT EXISTS res_state ON Resources(SimID ASC,StateID ASC);",
+		"CREATE INDEX IF NOT EXISTS comp_id ON Compositions(SimID ASC,ID ASC);",
+		"CREATE INDEX IF NOT EXISTS comp_iso ON Compositions(SimID ASC,IsoID ASC);",
+		"CREATE INDEX IF NOT EXISTS trans_id ON Transactions(SimID ASC,ID ASC);",
+		"CREATE INDEX IF NOT EXISTS trans_time ON Transactions(SimID ASC,Time ASC);",
+		"CREATE INDEX IF NOT EXISTS trans_receiver ON Transactions(SimID ASC,ReceiverID ASC);",
+		"CREATE INDEX IF NOT EXISTS transres_transid ON TransactedResources(SimID ASC,TransactionID ASC);",
+		"CREATE INDEX IF NOT EXISTS transres_resid ON TransactedResources(SimID ASC,ResourceID ASC);",
+		"CREATE INDEX IF NOT EXISTS rescreate_resid ON ResCreators(SimID ASC,ResID ASC);",
+		"CREATE INDEX IF NOT EXISTS agent_proto ON Agents(SimID ASC,Prototype ASC);",
+		"CREATE INDEX IF NOT EXISTS agent_id ON Agents(SimID ASC,ID ASC);",
+		// simid indexes
+		//"CREATE INDEX IF NOT EXISTS res_simid ON Resources(SimID ASC,Parent1 ASC,Parent2 ASC);",
+		//"CREATE INDEX IF NOT EXISTS trans_simid ON Transactions(SimID ASC,ID ASC);",
+		//"CREATE INDEX IF NOT EXISTS transres_simid ON TransactedResources(SimID ASC,TransactionID ASC,ResourceID ASC);",
+		"CREATE INDEX IF NOT EXISTS simid_res ON Resources(SimID ASC);",
+		"CREATE INDEX IF NOT EXISTS simid_transres ON TransactedResources(SimID ASC);",
+		"CREATE INDEX IF NOT EXISTS simid_comp ON Compositions(SimID ASC);",
+		"CREATE INDEX IF NOT EXISTS simid_trans ON Transactions(SimID ASC);",
+		"CREATE INDEX IF NOT EXISTS simid_rescreate ON ResCreators(SimID ASC);",
+		"CREATE INDEX IF NOT EXISTS simid_agent ON Agents(SimID ASC);",
+	}
+	postExecStmts = []string{
+		"CREATE INDEX IF NOT EXISTS inv_agent ON Inventories(SimID ASC,AgentID ASC);",
+		"CREATE INDEX IF NOT EXISTS inv_start ON Inventories(SimID ASC,StartTime ASC);",
+		"CREATE INDEX IF NOT EXISTS inv_end ON Inventories(SimID ASC,EndTime ASC);",
+	}
+	dumpSql  = "INSERT INTO Inventories VALUES (?,?,?,?,?)"
+	resSql   = "SELECT ID,TimeCreated FROM Resources WHERE Parent1 = ? OR Parent2 = ? AND SimID = ?"
+	ownerSql = `SELECT tr.ReceiverID, tr.Time FROM Transactions AS tr
+				  INNER JOIN TransactedResources AS trr ON tr.ID = trr.TransactionID AND trr.SimID = tr.SimID
+				  WHERE trr.ResourceID = ? AND tr.SimID = ? ORDER BY tr.Time ASC;`
+	rootsSql = `SELECT res.ID,res.TimeCreated,rc.ModelID FROM Resources AS res
+				  INNER JOIN ResCreators AS rc ON res.ID = rc.ResID AND rc.SimID = res.SimID
+				  WHERE res.SimID = ?;`
+)
+
+func Prepare(conn *sqlite3.Conn) (err error) {
+	fmt.Println("Creating indexes and inventory table...")
+	for _, sql := range preExecStmts {
+		if err := conn.Exec(sql); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func Finish(conn *sqlite3.Conn) (err error) {
+	fmt.Println("Creating inventory indexes...")
+	for _, sql := range postExecStmts {
+		if err := conn.Exec(sql); err != nil {
+			return err
+		}
+	}
+	return nil
 }
 
 type Node struct {
@@ -45,8 +121,56 @@ type Node struct {
 	EndTime   int
 }
 
-func GetRoots(conn *sqlite3.Conn) (roots []*Node, err error) {
-	stmt, err := conn.Query("SELECT COUNT(*) FROM ResCreators")
+type Context struct {
+	*sqlite3.Conn
+	Simid     string
+	dumpStmt  *sqlite3.Stmt
+	resStmt   *sqlite3.Stmt
+	ownerStmt *sqlite3.Stmt
+	resCount  int
+	Nodes     []*Node
+}
+
+func (c *Context) init() (err error) {
+	c.Nodes = make([]*Node, 0, 10000)
+
+	c.dumpStmt, err = c.Prepare(dumpSql)
+	if err != nil {
+		return err
+	}
+	c.resStmt, err = c.Prepare(resSql)
+	if err != nil {
+		return err
+	}
+	c.ownerStmt, err = c.Prepare(ownerSql)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (c *Context) WalkAll() (err error) {
+	if err := c.init(); err != nil {
+		return err
+	}
+	fmt.Println("Retrieving root resource nodes...")
+	roots, err := c.getRoots()
+	if err != nil {
+		return err
+	}
+	fmt.Printf("Found %v root nodes\n", len(roots))
+	for i, n := range roots {
+		fmt.Printf("Processing root %d...\n", i)
+		if err := c.walkNode(n); err != nil {
+			return err
+		}
+	}
+	return c.dumpNodes()
+}
+
+func (c *Context) getRoots() (roots []*Node, err error) {
+	sql := "SELECT COUNT(*) FROM ResCreators WHERE SimID = ?"
+	stmt, err := c.Query(sql, c.Simid)
 	if err != nil {
 		return nil, err
 	}
@@ -56,9 +180,7 @@ func GetRoots(conn *sqlite3.Conn) (roots []*Node, err error) {
 	}
 
 	roots = make([]*Node, 0, n)
-	sql := `SELECT ID,TimeCreated,ModelID FROM Resources 
-	          INNER JOIN ResCreators ON ID = ResID`
-	for stmt, err = conn.Query(sql); err == nil; err = stmt.Next() {
+	for stmt, err = c.Query(rootsSql, c.Simid); err == nil; err = stmt.Next() {
 		node := &Node{EndTime: math.MaxInt32}
 		if err := stmt.Scan(&node.ResId, &node.StartTime, &node.OwnerId); err != nil {
 			return nil, err
@@ -71,90 +193,11 @@ func GetRoots(conn *sqlite3.Conn) (roots []*Node, err error) {
 	return roots, nil
 }
 
-var preExecStmts = []string{
-	"CREATE TABLE IF NOT EXISTS Inventories (ResID INTEGER,AgentID INTEGER,StartTime INTEGER,EndTime INTEGER);",
-	"CREATE INDEX IF NOT EXISTS res_id ON Resources(ID ASC);",
-	"CREATE INDEX IF NOT EXISTS res_par1 ON Resources(Parent1 ASC);",
-	"CREATE INDEX IF NOT EXISTS res_par2 ON Resources(Parent2 ASC);",
-	"CREATE INDEX IF NOT EXISTS res_state ON Resources(StateID ASC);",
-	"CREATE INDEX IF NOT EXISTS comp_id ON Compositions(ID ASC);",
-	"CREATE INDEX IF NOT EXISTS comp_iso ON Compositions(IsoID ASC);",
-	"CREATE INDEX IF NOT EXISTS trans_id ON Transactions(ID ASC);",
-	"CREATE INDEX IF NOT EXISTS trans_time ON Transactions(Time ASC);",
-	"CREATE INDEX IF NOT EXISTS trans_receiver ON Transactions(ReceiverID ASC);",
-	"CREATE INDEX IF NOT EXISTS transres_transid ON TransactedResources(TransactionID ASC);",
-	"CREATE INDEX IF NOT EXISTS transres_resid ON TransactedResources(ResourceID ASC);",
-	"CREATE INDEX IF NOT EXISTS rescreate_resid ON ResCreators(ResID ASC);",
-	"CREATE INDEX IF NOT EXISTS agent_proto ON Agents(Prototype ASC);",
-	"CREATE INDEX IF NOT EXISTS agent_id ON Agents(ID ASC);",
-}
-
-var postExecStmts = []string{
-	"CREATE INDEX IF NOT EXISTS inv_agent ON Inventories(AgentID ASC);",
-	"CREATE INDEX IF NOT EXISTS inv_start ON Inventories(StartTime ASC);",
-	"CREATE INDEX IF NOT EXISTS inv_end ON Inventories(EndTime ASC);",
-}
-
-const (
-	dumpSql  = "INSERT INTO Inventories VALUES (?,?,?,?)"
-	resSql   = "SELECT ID,TimeCreated FROM Resources WHERE Parent1 = ? OR Parent2 = ?"
-	ownerSql = `SELECT tr.ReceiverID, tr.Time FROM Transactions AS tr
-				  INNER JOIN TransactedResources AS trr ON tr.ID = trr.TransactionID
-				  WHERE trr.ResourceID = ? ORDER BY tr.Time ASC;`
-)
-
-type Context struct {
-	*sqlite3.Conn
-	dumpStmt  *sqlite3.Stmt
-	resStmt   *sqlite3.Stmt
-	ownerStmt *sqlite3.Stmt
-	resCount  int
-	Nodes     []*Node
-}
-
-func (c *Context) Init() (err error) {
-	c.Nodes = make([]*Node, 0, 10000)
-
-	fmt.Println("Creating indexes and inventory table...")
-	for _, sql := range preExecStmts {
-		if err := c.Exec(sql); err != nil {
-			return err
-		}
-	}
-
-	c.dumpStmt, err = c.Prepare(dumpSql)
-	if err != nil {
-		return err
-	}
-
-	c.resStmt, err = c.Prepare(resSql)
-	if err != nil {
-		return err
-	}
-
-	c.ownerStmt, err = c.Prepare(ownerSql)
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func (c *Context) Finish() (err error) {
-	fmt.Println("Creating inventory indexes...")
-	for _, sql := range postExecStmts {
-		if err := c.Exec(sql); err != nil {
-			return err
-		}
-	}
-	return nil
-}
-
-func (c *Context) WalkDown(node *Node) (err error) {
+func (c *Context) walkNode(node *Node) (err error) {
 	if err := c.walkDown(node); err != nil {
 		return err
 	}
-	return c.DumpNodes()
+	return nil
 }
 
 func (c *Context) walkDown(node *Node) (err error) {
@@ -166,14 +209,14 @@ func (c *Context) walkDown(node *Node) (err error) {
 	// dump if necessary
 	c.resCount++
 	if c.resCount%dumpfreq == 0 {
-		if err := c.DumpNodes(); err != nil {
+		if err := c.dumpNodes(); err != nil {
 			return err
 		}
 	}
 
 	// find resource's children and resource owners
 	kids := make([]*Node, 0, 2)
-	for err = c.resStmt.Query(node.ResId, node.ResId); err == nil; err = c.resStmt.Next() {
+	for err = c.resStmt.Query(node.ResId, node.ResId, c.Simid); err == nil; err = c.resStmt.Next() {
 		child := &Node{EndTime: math.MaxInt32}
 		if err := c.resStmt.Scan(&child.ResId, &child.StartTime); err != nil {
 			return err
@@ -217,7 +260,7 @@ func (c *Context) walkDown(node *Node) (err error) {
 
 func (c *Context) getNewOwners(id int) (owners, times []int, err error) {
 	var owner, t int
-	for err = c.ownerStmt.Query(id); err == nil; err = c.ownerStmt.Next() {
+	for err = c.ownerStmt.Query(id, c.Simid); err == nil; err = c.ownerStmt.Next() {
 		if err := c.ownerStmt.Scan(&owner, &t); err != nil {
 			return nil, nil, err
 		}
@@ -230,13 +273,13 @@ func (c *Context) getNewOwners(id int) (owners, times []int, err error) {
 	return owners, times, nil
 }
 
-func (c *Context) DumpNodes() (err error) {
+func (c *Context) dumpNodes() (err error) {
 	fmt.Printf("dumping inventories (%d resources done)\n", c.resCount)
 	if err := c.Exec("BEGIN TRANSACTION;"); err != nil {
 		return err
 	}
 	for _, n := range c.Nodes {
-		if err = c.dumpStmt.Exec(n.ResId, n.OwnerId, n.StartTime, n.EndTime); err != nil {
+		if err = c.dumpStmt.Exec(c.Simid, n.ResId, n.OwnerId, n.StartTime, n.EndTime); err != nil {
 			return err
 		}
 	}
